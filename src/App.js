ae9275614a992bac82955e03b4dbe2054d9a6b9d
Frank Merchlewitz <jlimhb@gmail.com>
2021-05-05 20:41:17 -0500

    Updated App.js and Index.js with general framing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path={["/", "/home"]}>
+          <Home />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/portfolio">
+          <Portfolio />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
+  </Router>
   );
 }
 
