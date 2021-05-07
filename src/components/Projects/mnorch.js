6516186cf8424b9dcb8445163208bf7fbf42fa9d
Frank Merchlewitz <jlimhb@gmail.com>
2021-05-06 23:29:34 -0500

    setting up different project templates for portfolio page

---
+++ src/components/Projects/mnorch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function MNOrch(props) {
+    return(
+        
+    )
+}
+
+export default resolveModuleNameFromCache;
---
+++ src/components/Projects/videos.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Videos(props) {
+    return(
+        
+    )
+}
+
+export default Videos
